b5910c0c75084006cb38d51e3da8463980e31431
Cinthia Sanchez <cinthiasanchez@Cinthias-MacBook-Pro.local>
2023-03-22 18:51:21 -0700

    all

---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,26 @@
+import '../styles/styles.css'
+export const Cards=()=>{
+    return(
+        <article className='card m-3'>
+            <img src='' className='card-img-top' alt='' />
+            <div className='card-body'>
+                <div className='card__userDetails'>
+                   <img src='./images/vincent.webp' alt=''/>
+                   <div className='d-flex flex-column'>
+                     <p className='fw-bold'>Alfred</p>
+                     <p className='time'>27 feb 2023</p>
+                   </div>
+                </div>
+                <a href='#'>Articule</a>
+                <h3 className='card-title ms-3'>title</h3>
+                <ul className='d-flex flex-wrap p-0 ms-4'>
+                    <li><a href='#'>#Webdev</a></li>
+                    <li><a href='#'>#Beginners</a></li>
+                </ul>
+                <div className='card__buttons d-flex ms-4'>
+                    
+                </div>
+            </div>
+        </article>
+    )
+}
---
+++ src/components/Main/Main.js
@@ -0,0 +1,28 @@
+import { Cards } from '../Cards'
+import '../styles/styles.css'
+export const Main=()=>{
+    return(
+        <section className="page-container container-xl d-flex justify-content-between">
+            <section className="page__leftColumn">Left</section>
+            <section className='page__centerColumn'>
+            <div className="container-lg crayons-header ps-0">
+            <ul className="nav tabContainer">
+                <li className="nav-item">
+                    <a className="nav-link fs-5 fw-bold text-dark" aria-current="page" href="#">Relevant</a>
+                </li>
+                <li class="nav-item">
+                     <a className="nav-link disabled fs-5" href="#">Latest</a>
+                </li>
+                <li class="nav-item">
+                    <a className="nav-link disabled fs-5">Top</a>
+                </li>
+            </ul>
+        </div>
+        <div id="articles" className="articles">
+            <Cards/>
+        </div>
+            </section>
+            <section className='page__righColumn'>Right </section>
+        </section>
+    )
+}

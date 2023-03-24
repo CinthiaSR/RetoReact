67f150a39b0e5a6dd4b38a8ad5b13266db78435f
MASC95 <miguelangel.serapio@iest.edu.mx>
2023-03-23 22:49:14 -0600

    Probando react router dom

--- src/App.js
+++ src/App.js
@@ -2,24 +2,12 @@
 // import './App.css';
 // import './components/styles/styles.css'
 import { Main } from "./components/Home/Main/Main";
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
       <Main/>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
 }
